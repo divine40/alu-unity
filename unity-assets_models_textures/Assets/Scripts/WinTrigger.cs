c7bb576ed3cfd7155e5d0b943f116172097941d3
Akunyiba Chimdalu Divinefavour <c.akunyiba@alustudent.com>
2025-06-09 11:15:07 +0200

    done

---
+++ unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Timer timerScript;
+    public Text timerText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timerScript.StopTimer();
+            timerText.fontSize = 60;
+            timerText.color = Color.green;
+        }
+    }
+        
+    }
